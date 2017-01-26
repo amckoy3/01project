775074b5187291615011689524a255ac7ee393aa
Ashley Mckoy <you@example.com>
2017-01-26 16:32:51 -0500

    2nd attemp to print reverse

--- StructuresHw1.rkt
+++ StructuresHw1.rkt
@@ -3,8 +3,8 @@
 (define (reverse-general L)
 	(cond((null? L) '()) ;IF EMPTY LIST, JUMP TO ELSE & PRINT EMPTY QOUTE
 		((list? L) ;TESTING IF NUMBERS ARE BEING READ
-		(append ( reverse-general (cdr L))
-			('())) ;QOUTE REVERSED LIST
+		(append ( reverse-general (cdr L)) ;TAKES REMAINING ITEMS 
+			(reverse-general '())) ;QOUTE REVERSED LIST
 		)
 		(else L)
 		)
