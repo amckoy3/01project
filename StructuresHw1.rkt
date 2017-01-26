d4676c77bf3dffbfb4e5fed097a00bed1f73269e
Ashley Mckoy <you@example.com>
2017-01-26 16:24:55 -0500

    attempt to reverse list

--- StructuresHw1.rkt
+++ StructuresHw1.rkt
@@ -2,9 +2,12 @@
 ;reverse-general function
 (define (reverse-general L)
 	(cond((null? L) '()) ;IF EMPTY LIST, JUMP TO ELSE & PRINT EMPTY QOUTE
-		((list? L)) ;TESTING IF NUMBERS ARE BEING READ
+		((list? L) ;TESTING IF NUMBERS ARE BEING READ
+		(append ( reverse-general (cdr L))
+			('())) ;QOUTE REVERSED LIST
+		)
 		(else L)
 		)
-	) ; returned true
+	) 
 
 
