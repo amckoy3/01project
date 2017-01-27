40690ddfead22e83071846d3365cc6cfb8718fb3
Ashley Mckoy <you@example.com>
2017-01-27 10:06:05 -0500

    comparing min vals L1

--- StructuresHw1.rkt
+++ StructuresHw1.rkt
@@ -14,8 +14,11 @@
 ;2)
 ; sum-up-numbers-simple function
 (define (sum-up-numbers-simple L)
+  ;CASE 1: IF EMPTY LIST
   (cond ((null? L) 0)
+        ;CASE 2: IF FIRST ITEM IN LIST IS NUMBER
     ((number? (car L))
+        ;ADD FIRST ITEM PLUS REMAINING
         (+(car L) (sum-up-numbers-simple (cdr L)))
               )
         ((sum-up-numbers-simple (cdr L)) ))
@@ -24,11 +27,16 @@
 ;3)
 ;sum-up-numbers-general function
 (define (sum-up-numbers-general L)
+  ;CASE 1: IF LIST IS EMPTY RETURN 0
   (cond ((null? L) 0)
+        ;CHECK FIRST ITEM IN LIST
         ((list? (car L))
+         ;ADD FIRST AND LAST NUMBERS OF LIST
          (+ (sum-up-numbers-general (car L)) (sum-up-numbers-general (cdr L))))
+        ;IF FIRST ITEM NON NUMBER ADD LAST NUM + 0
         ((not (number? (car L)))
          (+ 0 (sum-up-numbers-general (cdr L))))
+        ;ADD REMAINING OUTSIDE NESTED LOOP
         (else
          (+ (car L) (sum-up-numbers-general (cdr L)))
          )
@@ -39,16 +47,23 @@
 ;min in list 1 has to be greater than min in list 2
 ;min-above-min function
 (define (min-above-min L1 L2)
-  ;case 1: check if null
+  ;case 1: check if first list null
   (cond ((null? L1) #F)
         
-  ;case 2: if there is no number in L2
+  ;case 2: if L2 null compare min values in L1 
   ((null? L2)
-   (min (car L1) (car L2))
-         (list? (cdr L1) (cdr L2))
+   (null? (cdr L1)) (car L1))
+  ((< (car l1) (min (cdr L1))) (car L1))
+  (else (min(cdr L1)))
+              
+              
+   ;(< (car L1) (min (cdr L1)) (car L1)))
+  ;(else (min (cdr L1)))
+     ;(< (car L1) (cdr L1)))
+        ;(map min L1 L2)
   )
 )
-  )
+  
   
 
     
